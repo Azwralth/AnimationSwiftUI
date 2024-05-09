5db4b0416e54fa2c6b57e5fd31d14a9eefc962d6
Vladislav <mackva34@gmail.com>
2024-05-09 13:45:21 +0200

    change

--- AnimationSwiftUI/ContentView.swift
+++ AnimationSwiftUI/ContentView.swift
@@ -8,47 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var iphoneView = false
-    @State private var rectangleView = false
-    @State private var isPresented = false
+    @State private var earAnimation = false
     
     var body: some View {
         ZStack {
-            Color(.lavanda)
+            Color(.cyan)
                 .ignoresSafeArea()
             
-            PhoneView(isPresented: isPresented)
-                .frame(width: 350, height: 700)
-                .transition(.move(edge: .bottom))
-                .offset(y: iphoneView ? 0 : UIScreen.main.bounds.height)
-                .animation(.easeInOut(duration: 1.5), value: iphoneView)
-            
-            if rectangleView {
-                CardView()
-                    .frame(width: 300, height: 180)
-                    .animation(.linear, value: rectangleView)
-                    .offset(x: rectangleView ? 0 : UIScreen.main.bounds.height, y: -120)
-                    .transition(.move(edge: .trailing))
-            }
-        }
-        .onTapGesture {
-            activateAnimation()
-        }
-    }
-    
-    private func activateAnimation() {
-        withAnimation {
-            iphoneView.toggle()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
-                withAnimation {
-                    isPresented.toggle()
+            PathAnimation(earAnimation: earAnimation)
+                .frame(width: 400, height: 700)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 2).repeatForever()) {
+                        earAnimation.toggle()
+                    }
                 }
-            }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation {
-                    rectangleView.toggle()
-                }
-            }
         }
     }
 }
--- AnimationSwiftUI/PathAnimation.swift
+++ AnimationSwiftUI/PathAnimation.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PathAnimation: View {
+    let earAnimation: Bool
+    
     var body: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
@@ -16,7 +18,56 @@ struct PathAnimation: View {
             let middle = size / 2
             let nearLine = size * 0.1
             let farLine = size * 0.9
+            let sunRadius = size * 0.2
             
+            
+            // MARK: - солнце
+            Path { path in
+                path.addArc(
+                    center: CGPoint(x: middle - nearLine * 2.5, y: nearLine),
+                    radius: sunRadius,
+                    startAngle: .degrees(0),
+                    endAngle: .degrees(360),
+                    clockwise: false
+                )
+            }
+            .fill(Color.yellow)
+            .offset(x: earAnimation ? middle * 0.3 : middle * 0.1)
+            
+            // MARK: - левый луг
+            Path { path in
+                path.move(to: CGPoint(x: -size, y: size * 1.15))
+                path.addQuadCurve(
+                    to: CGPoint(x: size * 0.6, y: size * 1.5),
+                    control: CGPoint(x: middle, y: middle * 1.3)
+                )
+            }
+            .fill(.green)
+            .shadow(radius: 20)
+            .offset(y: earAnimation ? middle * 0.3 : middle * 0.2)
+            
+            // MARK: - правый луг
+            Path { path in
+                path.move(to: CGPoint(x: size * 1.3, y: size * 1.4))
+                path.addQuadCurve(
+                    to: CGPoint(x: middle * 0.3, y: size * 1.5),
+                    control: CGPoint(x: middle * 2.5, y: middle * 0.6)
+                )
+            }
+            .fill(.green)
+            .shadow(radius: 20)
+            .offset(y: earAnimation ? middle * 0.3 : middle * 0.2)
+            
+            // MARK: - нижний луг
+            Path { path in
+                path.move(to: CGPoint(x: -size, y: size * 1.75))
+                path.addQuadCurve(to: CGPoint(x: size * 1.5, y: size * 1.75), control: CGPoint(x: middle, y: middle * 1.3))
+            }
+            .fill(.green)
+            .shadow(radius: 10)
+            .offset(y: earAnimation ? middle * 0.3 : middle * 0.2)
+            
+            // MARK: - левое ухо
             Path { path in
                 path.move(to: CGPoint(x: middle - nearLine * 3, y: nearLine))
                 path.addLine(to: CGPoint(x: nearLine * 3.5, y: nearLine * 3.5))
@@ -25,9 +76,23 @@ struct PathAnimation: View {
                 path.addLine(to: CGPoint(x: nearLine, y: middle * 0.75))
                 path.closeSubpath()
             }
-            .fill(.white)
-            .stroke(.black, style: StrokeStyle())
+            .fill(.gray)
+            .stroke(.gray, style: StrokeStyle())
+            .shadow(radius: 5)
             
+            // MARK: - левое ухо середина
+            Path { path in
+                path.move(to: CGPoint(x: middle - nearLine * 2.8, y: nearLine * 2.4))
+                path.addLine(to: CGPoint(x: nearLine * 2.8, y: nearLine * 3.4))
+                path.addLine(to: CGPoint(x: middle - nearLine * 2.8, y: nearLine * 4.2))
+                path.addLine(to: CGPoint(x: nearLine * 1.7, y: nearLine * 3.4))
+                path.closeSubpath()
+            }
+            .fill(.myPink)
+            .stroke(.myPink, style: StrokeStyle())
+            .offset(x: middle * 0.01, y: middle * 0.4)
+            
+            // MARK: - правое ухо
             Path { path in
                 path.move(to: CGPoint(x: middle * 1.2, y: nearLine))
                 path.addLine(to: CGPoint(x: width - nearLine * 5.9, y: nearLine * 3.5))
@@ -36,8 +101,36 @@ struct PathAnimation: View {
                 path.addLine(to: CGPoint(x: nearLine * 6.4, y: middle * 0.75))
                 path.closeSubpath()
             }
-            .fill(.white)
-            .stroke(.black, style: StrokeStyle())
+            .fill(.gray)
+            .stroke(.gray, style: StrokeStyle())
+            .shadow(radius: 5)
+            
+            // MARK: - правое ухо середина
+            
+            Path { path in
+                path.move(to: CGPoint(x: middle - nearLine * 2.8, y: nearLine * 2.4))
+                path.addLine(to: CGPoint(x: nearLine * 2.8, y: nearLine * 3.4))
+                path.addLine(to: CGPoint(x: middle - nearLine * 2.8, y: nearLine * 4.2))
+                path.addLine(to: CGPoint(x: nearLine * 1.7, y: nearLine * 3.4))
+                path.closeSubpath()
+            }
+            .fill(.myPink)
+            .stroke(.myPink, style: StrokeStyle())
+            .offset(x: middle * 0.6, y: middle * 0.4)
+        
+            // MARK: - тело
+            Path { path in
+                path.move(to: CGPoint(x: nearLine * 6, y: nearLine * 9.6))
+                path.addLine(to: CGPoint(x: middle * 1.8, y: nearLine * 11))
+                path.addLine(to: CGPoint(x: middle * 1.9, y: nearLine * 16))
+                path.addLine(to: CGPoint(x: middle * 1.84, y: nearLine * 16.9))
+                path.addLine(to: CGPoint(x: nearLine * 1.8, y: nearLine * 16.9))
+                path.addLine(to: CGPoint(x: nearLine * 2.9, y: middle + farLine * 1.1))
+                path.addLine(to: CGPoint(x: nearLine * 2.1, y: nearLine * 10.33))
+                path.closeSubpath()
+            }
+            .fill(.gray)
+//            .stroke(.black, style: StrokeStyle())
             
             Path { path in
                 path.move(to: CGPoint(x: nearLine * 1.9, y: farLine * 0.79))
@@ -50,6 +143,8 @@ struct PathAnimation: View {
             .fill(.white)
             .stroke(.black, style: StrokeStyle())
             
+            
+            
             Path { path in
                 path.move(to: CGPoint(x: nearLine * 1.9, y: farLine * 0.79))
                 path.addLine(to: CGPoint(x: middle - nearLine * 1.87, y: nearLine * 11.5))
@@ -57,20 +152,10 @@ struct PathAnimation: View {
                 path.move(to: CGPoint(x: nearLine * 6, y: nearLine * 9.6))
                 path.addLine(to: CGPoint(x: nearLine * 0.9, y: middle * 1.8))
             }
-            .fill(.white)
-            .stroke(.black, style: StrokeStyle())
+            .fill(.gray)
+//            .stroke(.black, style: StrokeStyle())
             
-            Path { path in
-                path.move(to: CGPoint(x: nearLine * 6, y: nearLine * 9.6))
-                path.addLine(to: CGPoint(x: middle * 1.8, y: nearLine * 11))
-                path.addLine(to: CGPoint(x: middle * 1.9, y: nearLine * 16))
-                path.addLine(to: CGPoint(x: middle * 1.84, y: nearLine * 16.9))
-                path.addLine(to: CGPoint(x: nearLine * 1.8, y: nearLine * 16.9))
-                path.addLine(to: CGPoint(x: nearLine * 2.9, y: middle + farLine * 1.1))
-                path.addLine(to: CGPoint(x: nearLine * 2.1, y: nearLine * 10.33))
-            }
-            .fill(.clear)
-            .stroke(.black, style: StrokeStyle())
+            
             
             Path { path in
                 path.move(to: CGPoint(x: nearLine * 6, y: nearLine * 9.6))
@@ -78,8 +163,8 @@ struct PathAnimation: View {
                 path.addLine(to: CGPoint(x: nearLine * 5.5, y: nearLine * 16.9))
                 path.addLine(to: CGPoint(x: middle * 1.8, y: nearLine * 11))
             }
-            .fill(.clear)
-            .stroke(.black, style: StrokeStyle())
+            .fill(.gray)
+//            .stroke(.black, style: StrokeStyle())
             
             Path { path in
                 path.move(to: CGPoint(x: nearLine * 6, y: nearLine * 9.6))
@@ -87,13 +172,13 @@ struct PathAnimation: View {
                 path.move(to: CGPoint(x: nearLine * 2.9, y: middle + farLine * 1.1))
                 path.addLine(to: CGPoint(x: nearLine * 3.15, y: nearLine * 16.25))
             }
-            .fill(.clear)
+            .fill(.white)
             .stroke(.black, style: StrokeStyle())
         }
     }
 }
 
 #Preview {
-    PathAnimation()
+    PathAnimation(earAnimation: false)
         .frame(width: 400, height: 700)
 }
